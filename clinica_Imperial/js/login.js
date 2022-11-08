e97bd64082905e0ffc3b0ac0b6f5f39b8a37c742
jbMendozaRomero666 <112987708+jbMendozaRomero666@users.noreply.github.com>
2022-11-08 17:57:56 -0500

    Desarrollo market place

---
+++ clinica_Imperial/js/login.js
@@ -0,0 +1,31 @@
+const formulario = document.querySelector('#formulario');
+function validar() {
+
+    //pruebas
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    if (email == "correo@correo.com" && password == "1234") {
+        document.form.submit();
+    } else {
+        LimpiarAlerta();
+        //generar el html
+        const error = document.createElement('P');
+        error.textContent = 'correo o password invalido Intente de nuevo';
+        error.classList.add('error');
+        //inyectar el error al fomulario
+        formulario.appendChild(error);
+
+        setTimeout(() => {
+            error.remove();
+        }, 3000);
+    }
+}
+
+function LimpiarAlerta(){
+    const alerta = document.querySelector('.error');
+
+    if(alerta){
+        alerta.remove();
+    }
+}
